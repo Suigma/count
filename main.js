0f0f4011c30116713337e52f04812d163db35693
Suigma <163005623+Suigma@users.noreply.github.com>
2024-09-17 10:59:46 +0700

    Update main.js

--- main.js
+++ main.js
@@ -5,17 +5,17 @@ function countdown() {
         const now = new Date().getTime();
         const distance = countDownDate - now;
 
-        // Menghitung hari, jam, menit, dan detik
+        
         const days = Math.floor(distance / (1000 * 60 * 60 * 24));
         const hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
         const minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
         const seconds = Math.floor((distance % (1000 * 60)) / 1000);
 
-        // Menampilkan hasil dalam elemen HTML
+        
         document.getElementById("date").innerHTML = days + "d " + hours + "h "
             + minutes + "m " + seconds + "s ";
 
-        // Jika waktu habis
+    
         if (distance < 0) {
             clearInterval(x);
             document.getElementById("demo").innerHTML = "EXPIRED";
