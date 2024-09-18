230e1cce91c9f68e2bbe9a329ca3d2be65dd995b
Suigma <163005623+Suigma@users.noreply.github.com>
2024-09-18 07:40:02 +0700

    adding style.css

--- main.js
+++ main.js
@@ -1,3 +1,5 @@
+document.getElementById("header").innerHTML = "Gtw Prediksi aj";
+
 function countdown() {
     const countDownDate = new Date("December 5, 2024 00:00:00").getTime();
 
@@ -5,20 +7,17 @@ function countdown() {
         const now = new Date().getTime();
         const distance = countDownDate - now;
 
-        
         const days = Math.floor(distance / (1000 * 60 * 60 * 24));
         const hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
         const minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
         const seconds = Math.floor((distance % (1000 * 60)) / 1000);
 
-        
         document.getElementById("date").innerHTML = days + "d " + hours + "h "
             + minutes + "m " + seconds + "s ";
 
-    
         if (distance < 0) {
             clearInterval(x);
-            document.getElementById("demo").innerHTML = "EXPIRED";
+            document.getElementById("date").innerHTML = "Time's up";
         }
     }, 1000);
 }
